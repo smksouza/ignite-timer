03b9ff73555ad2ddb8db0b1ec3c2ee289c515e2c
Samuel Souza <smksouza.dev@gmail.com>
2023-09-06 16:34:50 -0300

    ignite-timer: criação de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import { ThemeProvider } from "styled-components";
-import { Button } from "./components/Button";
-import { defaultTheme } from "./styles/themes/default";
-import { GlobalStyle } from "./styles/themes/global";
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/themes/global'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
-  );
+  )
 }
