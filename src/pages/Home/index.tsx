bc4d9fe5b2a8fc2ae4926998f4b6b61b162f8d30
Samuel Souza <smksouza.dev@gmail.com>
2023-09-08 19:31:26 -0300

    estilização do input Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,12 +15,25 @@ export const Home = () => {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+          />
+          <datalist id="task-suggestions">
+            <option value="Projeto 1"></option>
+            <option value="Projeto 2"></option>
+            <option value="Projeto 3"></option>
+            <option value="test"></option>
+          </datalist>
           <label htmlFor="">durante</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
           <span>minutos.</span>
         </FormContainer>
